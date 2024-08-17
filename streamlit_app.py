74eab1da38ea331ca2dcf3d3c7873ef74b60e2f1
Rayyan Saqib <127650930+rayyanmoin@users.noreply.github.com>
2024-08-17 10:05:50 +0500

    SideBar

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,6 @@ st.info('Your Interview Prepration oF Your Future Job')
 with st.expander('Accordian'):
   st.write('This is the text inside the Accordian')
 
-
+with st.sidebar:
+  st.radio('Select your Gander : ',['Male','Female','other'])
+  
